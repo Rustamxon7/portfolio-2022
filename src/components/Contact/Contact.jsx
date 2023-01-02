1d5c9f0afd34f31f88e6c9bcc1e2cd167cd60894
rustamtolipov <rustamxontolipov788@gmail.com>
2023-01-03 04:55:26 +0500

    ✅

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,7 +6,7 @@ import P from '../UI/P';
 import './Contact.scss';
 
 const Contact = () => {
-  const [state, handleSubmit] = useForm('meqnongr');
+  const [state, handleSubmit] = useForm('xzbyaeyk');
 
   return (
     <div className='contact-section' id='contact'>
